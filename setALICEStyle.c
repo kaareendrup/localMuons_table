fcf3786f771beaa889368739854e9a952f07c1f2
Kaare Endrup Iversen <kaare.endrup@gmail.com>
2026-02-19 10:12:53 +0100

    Add label function with dynamic cuts information

--- setALICEStyle.c
+++ setALICEStyle.c
@@ -64,7 +64,7 @@ void drawLabel(TString MC_name, TString type, Double_t x = 0.50, Double_t y = 0.
         // details.push_back("matchedQualityCuts");
         details.push_back("matchedMchMid");
         details.push_back("muonQualityCuts");
-        } else if (MC_name == "HF") {
+    } else if (MC_name == "HF") {
         label = "Pythia HF C#rightarrow#mu MC";
         details.push_back("#sqrt{#it{s}} = 13.6 TeV");
         details.push_back("matchedQualityCuts");
@@ -109,5 +109,107 @@ void drawLabel(TString MC_name, TString type, Double_t x = 0.50, Double_t y = 0.
     }
     if (extra_label != "") {
         latex.DrawLatex(x, y - 0.05 * (details.size() + 1), extra_label);
+    }
+}
+
+void drawLabel_cuts(
+    TString MC_name, 
+    TString type, 
+    std::vector<float> *pTCuts, 
+    std::vector<float> *etaCuts,
+    Double_t x = 0.50, 
+    Double_t y = 0.85, 
+    bool ralign = true
+) {
+
+    TString label;
+    std::vector<TString> details;
+
+    // Add reco/gen to detailed info
+    if (type == "reco") {
+        details.push_back("Reconstructed");
+    } else if (type == "gen") {
+        details.push_back("Generator level");
+    } else if (type == "") {
+        // Do nothing
+    } else {
+        details.push_back(type);
+    }
+
+    // Add dataset info
+    if (MC_name == "DQ") {
+        label = "Pythia DQ prompt J/#Psi MC";
+        details.push_back("#sqrt{#it{s}} = 13.6 TeV");
+        // details.push_back("matchedQualityCuts");
+        details.push_back("matchedMchMid");
+        details.push_back("muonQualityCuts");
+    } else if (MC_name == "k4h_baseline") {
+        label = "Pythia General purpose MC";
+        details.push_back("#sqrt{#it{s}} = 13.6 TeV");
+        details.push_back("matchedQualityCuts");
+    } else if (MC_name == "k4h_standalone") {
+        label = "Pythia General purpose MC";
+        details.push_back("#sqrt{#it{s}} = 13.6 TeV");
+        details.push_back("matchedMchMid");
+        details.push_back("muonQualityCuts");
+    } else if (MC_name == "DQ_data") {
+        label = "2024 pp data";
+        details.push_back("#sqrt{#it{s}} = 13.6 TeV");
+        details.push_back("matchedMchMid");
+        details.push_back("muonQualityCuts");
+    } else {
+        label = MC_name;
+    }
+
+    int cuts_added = 0;
+    // Add cut info
+    for (int i = 0; i < 2; ++i) {
+        TString particle = (i == 0) ? "trig" : "assoc";
+        TString pT_str = Form("p_{T,%s}", particle.Data());
+        TString eta_str = Form("#eta_{%s}", particle.Data());
+
+        // pT cuts
+        if (pTCuts->at(2*i+0) > 0) {
+            pT_str = Form("%.1f < %s", pTCuts->at(2*i+0), pT_str.Data());
+        }
+        if (pTCuts->at(2*i+1) < 20) {
+            pT_str = Form("%s < %.1f", pT_str.Data(), pTCuts->at(2*i+1));
+        }
+
+        // Eta cuts
+        if (etaCuts->at(2*i+0) > -4.0) {
+            eta_str = Form("%.1f < %s", etaCuts->at(2*i+0), eta_str.Data());
+        }
+        if (etaCuts->at(2*i+1) < 4.0) {
+            eta_str = Form("%s < %.1f", eta_str.Data(), etaCuts->at(2*i+1));
+        }
+
+        if (pT_str != Form("p_{T,%s}", particle.Data())) {
+            details.push_back(pT_str + " GeV/c");
+            cuts_added++;
+        }
+        if (eta_str != Form("#eta_{%s}", particle.Data())) {
+            details.push_back(eta_str);
+            cuts_added++;
+        }
+    }
+
+    TLatex latex;
+    latex.SetNDC();
+    if (ralign) {
+        latex.SetTextAlign(31);
+    } else {
+        latex.SetTextAlign(11);
+    }
+    latex.SetTextSize(0.035);
+    latex.SetTextFont(42);
+    latex.DrawLatex(x, y, label);
+    for (size_t i = 0; i < details.size(); ++i) {
+        float y_offset = 0.05 * (i + 1);
+        if (i > details.size() - cuts_added) {
+            // Add extra spacing between cut details
+            y_offset = 0.05 * (details.size() - cuts_added + 1) + 0.06 * (i - (details.size() - cuts_added)); 
+        }
+        latex.DrawLatex(x, y - y_offset, details[i]);
     }
 }
