83a26ceeddd6d10e37f0dd45a7353bdcb7f9556a
Kaare Endrup <kaare.endrup@gmail.com>
2026-03-16 16:32:38 +0100

    Adap to multifile and new data structure

--- count_events.c
+++ count_events.c
@@ -8,23 +8,28 @@ TTree* get_tree(TKey *key, TFile *file, TString treeName) {
     return tree;
 }
 
-void count_input(TString MC_name) {
+void count_input(TString MC_name, int n_files) {
 
     // Load input data file list
-    TString input_file_list = "input_data/" + MC_name + "/input_data_local.txt";
+    TString input_file_list = TString::Format("input_data/%s/input_data_%d.txt", MC_name.Data(), n_files);
+
     std::ifstream infile(input_file_list.Data());
     std::string line;
 
     // Initialize event counting variable
     int event_count = 0;
     int muon_count = 0;
+    int gen_muon_count = 0;
+    Int_t pdgCode;
+    int file_count = 0;
 
     // Loop over input files
     while (std::getline(infile, line)) {
 
+        std::cout << TString::Format("Processing file %d of %d", file_count, n_files) << std::endl;
         TFile *file = TFile::Open(line.c_str());
         auto *keys = file->GetListOfKeys();
-        TTree *event_tree, *muon_tree;
+        TTree *event_tree, *muon_tree, *track_tree;
 
         // Loop over dataframes to count events and muons
         for (int i = 0; i < keys->GetEntries()-1; ++i) {
@@ -33,66 +38,92 @@ void count_input(TString MC_name) {
             muon_tree = get_tree((TKey*)keys->At(i), file, "O2reducedmuon");
             muon_count += muon_tree->GetEntries();
 
+            track_tree = get_tree((TKey*)keys->At(i), file, "O2reducedmctrack");
+            track_tree->SetBranchAddress("fPdgCode", &pdgCode);
+
+            for (Long64_t j = 0; j < track_tree->GetEntries(); ++j) {
+                track_tree->GetEntry(j);
+                if (abs(pdgCode) == 13) {
+                    gen_muon_count++;
+                }
+            }
+
             delete event_tree;
             delete muon_tree;
+            delete track_tree;
         }
+
+        file->Close();
+        file_count++;
     }
+    std::cout << std::endl;
 
     // Print the total number of unique events
     std::cout << "Input data stats for: " << MC_name << std::endl;
     std::cout << "  Total events: " << event_count << std::endl;
-    std::cout << "  Total muons: " << muon_count << std::endl << std::endl;
+    std::cout << "  Total reconstructed muon tracks (global and standalone): " << muon_count << std::endl;
+    std::cout << "  Total generated muons: " << gen_muon_count << std::endl << std::endl;
 }
 
-void count_analysis(TString MC_name) {
-    TString data_file = "results/" + MC_name + "/muonAOD.root";
-    
-    // Load the dataframe keys
-    TFile *file = TFile::Open(data_file);
-    auto *keys = file->GetListOfKeys();
-    
+void count_analysis(TString MC_name, int n_files) {
+
     // Initialize event counting variable
     int event_count = 0;
     int muon_count = 0;
-    
-    // Loop over dataframes
-    for (int i = 0; i < keys->GetEntries()-1; ++i) {
 
-        std::cout << "Processing " << i << "/" << keys->GetEntries()-1 << std::endl;
+    for (int f = 0; f < n_files; ++f) {
+        std::cout << "Processing file " << f << " of " << n_files << std::endl;
+        TString data_file = "results/" + MC_name + "/muonAOD" + f + ".root";
 
-        TTree *tree = get_tree((TKey*)keys->At(i), file, "O2dqmuontable");
-    
-        std::set<ULong64_t> unique_event_indices;
-        ULong64_t fEventIdx;
-        tree->SetBranchAddress("fEventIdx", &fEventIdx);
-        
-        Long64_t n = tree->GetEntries();
-        for (Long64_t j = 0; j < n; ++j) {
-            tree->GetEntry(j);
-            unique_event_indices.insert(fEventIdx);
+        // Load the dataframe keys
+        TFile *file = TFile::Open(data_file);
+        auto *keys = file->GetListOfKeys();
+
+        // Loop over dataframes
+        for (int i = 0; i < keys->GetEntries()-1; ++i) {
+
+            // std::cout << "Processing " << i << "/" << keys->GetEntries()-1 << std::endl;
+
+            TTree *tree = get_tree((TKey*)keys->At(i), file, "O2dqmuontable");
+
+            std::set<ULong64_t> unique_event_indices;
+            ULong64_t fEventIdx;
+            tree->SetBranchAddress("fEventIdx", &fEventIdx);
+            
+            Long64_t n = tree->GetEntries();
+            for (Long64_t j = 0; j < n; ++j) {
+                tree->GetEntry(j);
+                unique_event_indices.insert(fEventIdx);
+            }
+
+            event_count += unique_event_indices.size();
+            muon_count += n;
+
+            delete tree;
         }
-
-        event_count += unique_event_indices.size();
-        muon_count += n;
-
-        delete tree;
     }
-    
+    std::cout << std::endl;
+        
     // Print the total number of unique events
     std::cout << "Analysis data stats for: " << MC_name << std::endl;
     std::cout << "  Total events: " << event_count << std::endl;
     std::cout << "  Total muons: " << muon_count << std::endl << std::endl;
 }
 
-    
-void count_events() {
-    // TString MC_name = "DQ";
-    // TString MC_name = "HF";
-    // TString MC_name = "genpurp";
-    TString MC_name = "k4h_baseline/reco";
-    // TString MC_name = "k4h_standalone/reco";
-    // TString MC_name = "k4h_standalone/gen";
 
-    // count_input(MC_name);
-    count_analysis(MC_name);
+void count_events() {
+
+    // TString MC_name = "c3_global";
+    // count_input(MC_name, 200);
+    // TString MC_gen_name = "c3_global/gen";
+    // count_analysis(MC_gen_name, 25);
+    // TString MC_reco_name = "c3_global/reco";
+    // count_analysis(MC_reco_name, 25);
+    
+    TString MC_name = "c3_standalone";
+    count_input(MC_name, 200);
+    TString MC_gen_name = "c3_standalone/gen";
+    count_analysis(MC_gen_name, 25);
+    TString MC_reco_name = "c3_standalone/reco";
+    count_analysis(MC_reco_name, 25);
 }
