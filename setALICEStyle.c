f3a75d8e7c8adf29b219536e93b328cdef945863
Kaare Endrup <kaare.endrup@gmail.com>
2026-03-17 12:31:06 +0100

    Standardize cut function

--- setALICEStyle.c
+++ setALICEStyle.c
@@ -39,84 +39,11 @@ void increasePadMargins(TCanvas* c, int n) {
     }
 }
 
-void drawLabel(TString MC_name, TString type, Double_t x = 0.50, Double_t y = 0.85, bool ralign = true, TString extra_label = "") {
-
-    TString label;
-    std::vector<TString> details;
-
-    if (type == "reco") {
-        details.push_back("Reconstructed");
-    } else if (type == "gen") {
-        details.push_back("Generator level");
-    } else if (type == "") {
-        // Do nothing
-    } else {
-        details.push_back(type);
-    }
-
-    if (MC_name == "genpurp") {
-        label = "Pythia General purpose MC";
-        details.push_back("#sqrt{#it{s}} = 13.6 TeV");
-        details.push_back("matchedQualityCuts");
-    } else if (MC_name == "DQ") {
-        label = "Pythia DQ prompt J/#Psi MC";
-        details.push_back("#sqrt{#it{s}} = 13.6 TeV");
-        // details.push_back("matchedQualityCuts");
-        details.push_back("matchedMchMid");
-        details.push_back("muonQualityCuts");
-    } else if (MC_name == "HF") {
-        label = "Pythia HF C#rightarrow#mu MC";
-        details.push_back("#sqrt{#it{s}} = 13.6 TeV");
-        details.push_back("matchedQualityCuts");
-    } else if (MC_name == "k4h_baseline") {
-        label = "Pythia General purpose MC";
-        details.push_back("#sqrt{#it{s}} = 13.6 TeV");
-        details.push_back("matchedQualityCuts");
-    } else if (MC_name == "k4h_cuts") {
-        label = "Pythia General purpose MC";
-        details.push_back("#sqrt{#it{s}} = 13.6 TeV");
-        details.push_back("matchedQualityCuts");
-        details.push_back("#chi2 MFTMCH < 40");
-        details.push_back("Ncluster MFC > 5");
-        details.push_back("Ncluster MCH > 5");
-        details.push_back("-3.6 < #eta < -2.5");
-    } else if (MC_name == "k4h_standalone") {
-        label = "Pythia General purpose MC";
-        details.push_back("#sqrt{#it{s}} = 13.6 TeV");
-        details.push_back("matchedMchMid");
-        details.push_back("muonQualityCuts");
-    } else if (MC_name == "DQ_data") {
-        label = "2024 pp data";
-        details.push_back("#sqrt{#it{s}} = 13.6 TeV");
-        details.push_back("matchedMchMid");
-        details.push_back("muonQualityCuts");
-    } else {
-        label = MC_name;
-    }
-
-    TLatex latex;
-    latex.SetNDC();
-    if (ralign) {
-        latex.SetTextAlign(31);
-    } else {
-        latex.SetTextAlign(11);
-    }
-    latex.SetTextSize(0.04);
-    latex.SetTextFont(42);
-    latex.DrawLatex(x, y, label);
-    for (size_t i = 0; i < details.size(); ++i) {
-        latex.DrawLatex(x, y - 0.05 * (i + 1), details[i]);
-    }
-    if (extra_label != "") {
-        latex.DrawLatex(x, y - 0.05 * (details.size() + 1), extra_label);
-    }
-}
-
 void drawLabel_cuts(
     TString MC_name, 
     TString type, 
-    std::vector<float> *pTCuts, 
-    std::vector<float> *etaCuts,
+    std::vector<float> *pTCuts = nullptr, 
+    std::vector<float> *etaCuts = nullptr,
     Double_t x = 0.50, 
     Double_t y = 0.85, 
     bool ralign = true
@@ -178,39 +105,44 @@ void drawLabel_cuts(
     }
 
     int cuts_added = 0;
-    // Add cut info
-    // Cuts are specified as:
-    // {pT_trigger_min, pT_trigger_max, pT_assoc_min, pT_assoc_max}
-    // {eta_trigger_min, eta_trigger_max, eta_assoc_min, eta_assoc_max}
-    for (int i = 0; i < 2; ++i) {
-        // TString particle = (i == 0) ? "trig" : "assoc";
-        TString particle = (i == 0) ? "trig" : "#mu";
-        TString pT_str = Form("p_{T,%s}", particle.Data());
-        TString eta_str = Form("#eta_{%s}", particle.Data());
+    if (pTCuts && etaCuts) {
+        // Add cut info
+        // Cuts are specified as:
+        // {pT_trigger_min, pT_trigger_max, pT_assoc_min, pT_assoc_max}
+        // {eta_trigger_min, eta_trigger_max, eta_assoc_min, eta_assoc_max}
 
-        // pT cuts
-        if (pTCuts->at(2*i+0) > 0) {
-            pT_str = Form("%.1f < %s", pTCuts->at(2*i+0), pT_str.Data());
-        }
-        if (pTCuts->at(2*i+1) < 20) {
-            pT_str = Form("%s < %.1f", pT_str.Data(), pTCuts->at(2*i+1));
-        }
+        // Loop over mu and J/Psi
+        for (int i = 0; i < 2; ++i) {
+            // TString particle = (i == 0) ? "trig" : "assoc";
+            TString particle = (i == 0) ? "trig" : "#mu";
+            TString pT_str = Form("p_{T,%s}", particle.Data());
+            TString eta_str = Form("#eta_{%s}", particle.Data());
 
-        // Eta cuts
-        if (etaCuts->at(2*i+0) > -4.0) {
-            eta_str = Form("%.1f < %s", etaCuts->at(2*i+0), eta_str.Data());
-        }
-        if (etaCuts->at(2*i+1) < 4.0) {
-            eta_str = Form("%s < %.1f", eta_str.Data(), etaCuts->at(2*i+1));
-        }
+            // pT cuts
+            if (pTCuts->at(2*i+0) > 0) {
+                pT_str = Form("%.1f < %s", pTCuts->at(2*i+0), pT_str.Data());
+            }
+            if (pTCuts->at(2*i+1) < 20) {
+                pT_str = Form("%s < %.1f", pT_str.Data(), pTCuts->at(2*i+1));
+            }
 
-        if (pT_str != Form("p_{T,%s}", particle.Data())) {
-            details.push_back(pT_str + " GeV/c");
-            cuts_added++;
-        }
-        if (eta_str != Form("#eta_{%s}", particle.Data())) {
-            details.push_back(eta_str);
-            cuts_added++;
+            // Eta cuts
+            if (etaCuts->at(2*i+0) > -4.0) {
+                eta_str = Form("%.1f < %s", etaCuts->at(2*i+0), eta_str.Data());
+            }
+            if (etaCuts->at(2*i+1) < 4.0) {
+                eta_str = Form("%s < %.1f", eta_str.Data(), etaCuts->at(2*i+1));
+            }
+
+            // Check if cuts are non-trivial before adding to details
+            if (pT_str != Form("p_{T,%s}", particle.Data())) {
+                details.push_back(pT_str + " GeV/c");
+                cuts_added++;
+            }
+            if (eta_str != Form("#eta_{%s}", particle.Data())) {
+                details.push_back(eta_str);
+                cuts_added++;
+            }
         }
     }
 
