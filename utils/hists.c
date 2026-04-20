b8e54c0b9edd1f59aef362a85dda7503f95637e2
Kaare Endrup <kaare.endrup@gmail.com>
2026-04-20 17:03:36 +0200

    Common get weight function

--- utils/hists.c
+++ utils/hists.c
@@ -1,4 +1,6 @@
 
+#include <vector>
+
 double getRapidity(double pT, double eta) {
     double mJPsi = 3.096916; // J/Psi mass in GeV/c^2
     return log((sqrt(pow(mJPsi, 2) + (pow(pT, 2) * pow(cosh(eta), 2))) + pT * sinh(eta)) / (sqrt(pow(mJPsi, 2) + pow(pT, 2))));
@@ -21,4 +23,20 @@ void fillHist(const TString& name, std::map<TString, std::unique_ptr<TH1F>>& his
     }
 
     hists[name]->Fill(value, weight);
+}
+
+double get_weight(double pT, const std::vector<double>& pT_bins, const std::vector<double>& efficiency, double eta_trigger_min, double eta_trigger_max) {
+    
+    int eff_bin = -1;
+    for (size_t b = 0; b < pT_bins.size() - 1; ++b) {
+        if (pT >= pT_bins[b] && pT < pT_bins[b + 1]) {
+            eff_bin = b;
+            break;
+        }
+    }
+    if (eff_bin == -1) {
+        std::cerr << "Warning: pT " << pT << " is out of efficiency histogram range!\n";
+        return 1.0; // No correction if out of range
+    }
+    return 1.0 / (efficiency[eff_bin] * getDeltaY(pT, eta_trigger_min, eta_trigger_max));
 }
