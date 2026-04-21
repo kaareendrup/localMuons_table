b76b0877657630682ec38cc573a7c21e6a18ba53
Kaare Endrup <kaare.endrup@gmail.com>
2026-04-21 13:19:15 +0200

    Update to fit PbPb

--- helper_scripts/make_command.py
+++ helper_scripts/make_command.py
@@ -1,11 +1,19 @@
-def make_command(n_files, data_name, tracktype, recotype, MC, config_dir):
+def make_command(n_files, data_name, tracktype, recotype, MC, PbPb, config_dir):
+
+    datatype = "PbPb" if PbPb else ""
 
     if MC:
         base_command = f"o2-analysis-dq-efficiency-with-assoc -b --configuration json://{config_dir}/configuration_dqEfficiency_withAssoc_{tracktype}_{recotype}.json"
+    elif PbPb:
+        base_command = f"o2-analysis-dq-table-reader -b --configuration json://{config_dir}/configuration_dqTableReader_{tracktype}{datatype}.json | o2-analysis-dq-model-converter-event-extended -b --configuration json://{config_dir}/configuration_dqTableReader_{tracktype}{datatype}.json"
     else:
-        base_command = f"o2-analysis-dq-table-reader-with-assoc -b --configuration json://{config_dir}/configuration_dqTableReader_withAssoc_{tracktype}.json | o2-analysis-dq-model-converter-event-extended -b --configuration json://{config_dir}/configuration_dqTableReader_withAssoc_{tracktype}.json"
+        base_command = f"o2-analysis-dq-table-reader-with-assoc -b --configuration json://{config_dir}/configuration_dqTableReader_withAssoc_{tracktype}{datatype}.json | o2-analysis-dq-model-converter-event-extended -b --configuration json://{config_dir}/configuration_dqTableReader_withAssoc_{tracktype}{datatype}.json"
 
-    input_file = f"/home/kaareendrup/analysis/input_data/{data_name}_{tracktype}/input_data_{n_files}.txt"
+    if PbPb:
+        input_file = f"/home/kaareendrup/analysis/input_data/{data_name}/input_data_{n_files}.txt"
+    else:
+        input_file = f"/home/kaareendrup/analysis/input_data/{data_name}_{tracktype}/input_data_{n_files}.txt"
+
     command = f"{base_command} --aod-file @{input_file} --aod-writer-json {config_dir}/outputDirector_{recotype}.json"
 
     print("To run, run:")
@@ -13,18 +21,20 @@ def make_command(n_files, data_name, tracktype, recotype, MC, config_dir):
 
 config_dir = "/home/kaareendrup/analysis/localMuons_table/config"
 
-n_files = 1
+n_files = 10
 
-data_name = "c3"
+# data_name = "c3"
 # data_name = "f4d"
 # data_name = "DQ_data"
+data_name = "DQ_PbPb"
 
-tracktype = "global"
-# tracktype = "standalone"
+# tracktype = "global"
+tracktype = "standalone"
 
-# recotype = "reco"
-recotype = "gen"
+recotype = "reco"
+# recotype = "gen"
 
-MC = True
+MC = data_name in ["c3", "f4d"]
+PbPb = "PbPb" in data_name
 
-make_command(n_files, data_name, tracktype, recotype, MC, config_dir)
+make_command(n_files, data_name, tracktype, recotype, MC, PbPb, config_dir)
