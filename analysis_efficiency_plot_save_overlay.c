b9dff3dac3c3190c3b522b7bfbadc477da5b5545
Kaare Endrup <kaare.endrup@gmail.com>
2026-05-05 14:30:36 +0200

    Adapt style

--- analysis_efficiency_plot_save_overlay.c
+++ analysis_efficiency_plot_save_overlay.c
@@ -57,19 +57,30 @@ void analysis_efficiency_plot_save_overlay() {
 
     TCanvas *c1 = new TCanvas("c1", "J/Psi eff", 700, 600);
     gStyle->SetOptTitle(1);
+    c1->Divide(1,2);
+
+    c1->cd(1);
+    gPad->SetPad(0,0.3,1,1);    // Top 70%
     // JPsiEffHist_1->GetXaxis()->SetRangeUser(0, 10);
     
     drawHist(JPsiEffHist_1, "J/#Psi Efficiency;p_{T} (GeV/c);Eff_{J/#Psi}", kBlue, 1.2);
     drawHist(JPsiEffHist_2, "J/#Psi Efficiency;p_{T} (GeV/c);Eff_{J/#Psi}", kRed, 1.2, true);
 
     // Add legend
-    TLegend *legend = new TLegend(0.5, 0.7, 0.85, 0.85);
+    TLegend *legend = new TLegend(0.42, 0.7, 0.75, 0.85);
     legend->AddEntry(JPsiEffHist_1, "DQ MC", "l");
     legend->AddEntry(JPsiEffHist_2, "General purpose MC", "l");
     legend->Draw();
     setMax({JPsiEffHist_1, JPsiEffHist_2});
 
-    drawLabel_cuts(MC_name_1, "", &pT_JPsi_cuts, &eta_JPsi_cuts, 0.49, 0.79);
-    c1->SaveAs(TString::Format("results/%s/efficiency_plots_overlay.png", MC_name_1.Data()));
+    drawLabel_cuts(MC_name_1, "", &config, 0.4, 0.79);
+    gPad->SetBottomMargin(0); // Remove bottom margin for top pad
 
+    c1->cd(2);
+    gStyle->SetOptTitle(0);
+    TH1F *ratio_hist = createRatioPlot(JPsiEffHist_1, JPsiEffHist_2);
+    ratio_hist->SetMinimum(0);
+    ratio_hist->SetMaximum(2);
+
+    c1->SaveAs(TString::Format("results/%s/efficiency_plots_overlay.png", MC_name_1.Data()));
 }
