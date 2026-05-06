8c353cafa7db10c474cb786d12460e2e9231f271
Kaare Endrup <kaare.endrup@gmail.com>
2026-05-06 14:27:33 +0200

    Use utils

--- analysis_efficiency.c
+++ analysis_efficiency.c
@@ -18,13 +18,7 @@
 #include <nlohmann/json.hpp>
 using json = nlohmann::json;
 
-bool charm_beauty_cut(Long64_t motherPDG) {
-    return ((std::abs(motherPDG) == 443) || (std::abs(motherPDG) == 100443) ||
-            (std::abs(motherPDG) >= 411 && std::abs(motherPDG) <= 445) || 
-            (std::abs(motherPDG) >= 4101 && std::abs(motherPDG) <= 4444) || 
-            (std::abs(motherPDG) >= 511 && std::abs(motherPDG) <= 557) || 
-            (std::abs(motherPDG) >= 5101 && std::abs(motherPDG) <= 5554));
-}
+#include "utils/data.c"
 
 void analysis_efficiency() {
 
